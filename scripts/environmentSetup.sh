b3aacc0838325ee761d13c290dbb3417420c68a1
ibilal <ibilal@pdx.edu>
2016-10-18 20:18:29 -0700

    environmentSetup.sh

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -8,23 +8,14 @@ function getAndroidSDK {
 
   if [ ! -e $DEPS ]; then
     echo y | android update sdk -u -a -t android-19 &&
-    echo y | android update sdk --no-ui --all --filter "android-24" &&
-    echo y | android update sdk -u -a -t platform-tools &&
+    echo y | android update sdk --no-ui --all --filter android-24 &&
     echo y | android update sdk --no-ui --filter build-tools-24.0.0 &&
-    echo y | android update sdk -u -a -t "extra-android-m2repository" &&
+    echo y | android update sdk -u -a -t platform-tools &&
+    echo y | android update sdk -u --no-ui --filter "extra-android-m2repository" &&
     echo y | android update sdk -u -a -t "extra-android-support" &&
-    echo y | android update sdk -u -a -t "extra-google-m2repository" &&
-    echo no | android create avd -n testAVD -f -t android-24 --abi google_apis/armeabi-v7a &&
+    echo y | android update sdk -u --no-ui --filter "extra-google-m2repository" &&
+  #  echo no | android create avd -n testAVD -f -t android-24 --abi google_apis/armeabi-v7a &&
     touch $DEPS
   fi
 }
 
-function waitForAVD {
-  local bootanim=""
-  export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
-  until [[ "$bootanim" =~ "stopped" ]]; do
-    sleep 5
-    bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
-    echo "emulator status=$bootanim"
-  done
-}
